3507e63c4ec0c69b73c5b263902d6390ebfc0f6c
mattyanayaka <104978029+mattyanayaka@users.noreply.github.com>
2025-11-23 11:03:01 +0900

    Add files via upload

--- portfolio/telme/urls.py
+++ portfolio/telme/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
-from . import views
+from django.conf.urls import url
+from .views import TelmeView
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    url(r'', TelmeView.as_view(), name='index'),
 ]
--- portfolio/telme/views.py
+++ portfolio/telme/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
+from .forms import HelloForm
 
-def index(request):
-    return render(request, 'telme/index.html')
+class TelmeView(TemplateView):
+    def __init__(self):
+        self.params = {
+            'title': 'Hello',
+            'message': 'your data:',
+            'form': HelloForm()
+        }
+
+    def get(self, request):
+        return render(request, 'telme/index.html', self.params)
+    
+    def post(self, request):
+        ch = request.POST.getlist('choice')
+        self.params['result'] = 'selected: ' + str(ch) + '.'
+        self.params['form'] = HelloForm(request.POST)
+        return render(request, 'telme/index.html', self.params)
