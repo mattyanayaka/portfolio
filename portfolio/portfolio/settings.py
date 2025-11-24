32349f92f8d0601ba5359bda617457f49d2cc665
mattyanayaka <104978029+mattyanayaka@users.noreply.github.com>
2025-11-24 16:44:13 +0900

    Add files via upload

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -22,10 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zel0_k(fwm7wf5+=w8^19%qir1g_1y4^+&i)0_jpqzkm8zrc2='
+# SECRET_KEY = 'django-insecure-zel0_k(fwm7wf5+=w8^19%qir1g_1y4^+&i)0_jpqzkm8zrc2='
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = ['localhost',
                  'portfolio-jja0.onrender.com']
